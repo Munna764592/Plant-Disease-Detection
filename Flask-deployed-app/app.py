ac3490a220d8694cc61811ebdb2706bb6323e135
Munna Kumar Pandit <96653272+Munna764592@users.noreply.github.com>
2024-04-24 18:00:53 +0530

    Add files via upload

---
+++ Flask-deployed-app/app.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask, redirect, render_template, request
+from PIL import Image
+import torchvision.transforms.functional as TF
+import CNN
+import numpy as np
+import torch
+import pandas as pd
+
+
+disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
+
+model = CNN.CNN(39)    
+model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.eval()
+
+def prediction(image_path):
+    image = Image.open(image_path)
+    image = image.resize((224, 224))
+    input_data = TF.to_tensor(image)
+    input_data = input_data.view((-1, 3, 224, 224))
+    output = model(input_data)
+    output = output.detach().numpy()
+    index = np.argmax(output)
+    return index
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        image = request.files['image']
+        filename = image.filename
+        file_path = os.path.join('static/uploads', filename)
+        image.save(file_path)
+        print(file_path)
+        pred = prediction(file_path)
+        title = disease_info['disease_name'][pred]
+        description =disease_info['description'][pred]
+
+        image_url = disease_info['image_url'][pred]
+
+        return render_template('submit.html' , title = title , desc = description , 
+                               image_url = image_url , pred = pred )
+
+if __name__ == '__main__':
+    app.run(debug=True)
